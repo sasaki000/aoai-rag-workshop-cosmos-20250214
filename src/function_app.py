443e7ea8de1c7e533b858f29a7fefca49bb542f6
MiuOnishi-kccs <s1320113s@gmail.com>
2025-02-14 08:23:12 +0000

    add chat_api

--- src/function_app.py
+++ src/function_app.py
@@ -1,3 +1,4 @@
+import json
 import os
 import azure.functions as func
 import logging
@@ -32,3 +33,45 @@ def indexer(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+
+@app.route(route="chat")
+def chat(req: func.HttpRequest) -> func.HttpResponse:
+        question = req.params.get("q")
+        content = vector_search(question)
+        answer = generate_answer(question, content)
+        return func.HttpResponse(
+             answer,
+             status_code=200
+        )
+
+
+def vector_search(question):
+        question_vector = aoai_client.embeddings.create(input=question, model=os.environ.get("AI_DEPLOYMENT_EMBEDDING")).data[0].embedding
+        search_results = container_client.query_items( 
+                query='SELECT TOP @k c.id, c.title, c.content, VectorDistance(c.contentVector,@embedding) AS SimilarityScore FROM c ORDER BY VectorDistance(c.contentVector,@embedding)', 
+                parameters=[ 
+                        {"name": "@k", "value": 3},
+                        {"name": "@embedding", "value": question_vector}
+                ], 
+                enable_cross_partition_query=True)
+        return json.dumps(list(search_results), ensure_ascii=False)
+
+system_message ="""
+あなたはMicrosoftAzureの専門家です。MicrosoftAzureの質問にのみ回答してください。
+回答には、以下の content からのみ回答してください。
+
+## content
+{content}
+"""
+
+def generate_answer(question, content):
+        messages = [
+                {"role": "system", "content": system_message.format(content=content)},
+                {"role": "user", "content": question}
+        ]
+        response = aoai_client.chat.completions.create(
+                messages=messages,
+                model = os.environ.get("AI_DEPLOYMENT_CHAT")
+        )
+        return response.choices[0].message.content
